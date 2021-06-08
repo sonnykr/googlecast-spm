686ba3ab738dfa824f6211e41893bd4e72bc9f13
Sonny <sonny@nowplayingapps.com>
2021-06-07 21:55:56 -0400

    Added framework

--- Package.swift
+++ Package.swift
@@ -18,11 +18,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "googlecast-spm",
-            dependencies: []),
-        .testTarget(
-            name: "googlecast-spmTests",
-            dependencies: ["googlecast-spm"]),
+            url: "",
+            checksum: ""
+        )
     ]
 )
